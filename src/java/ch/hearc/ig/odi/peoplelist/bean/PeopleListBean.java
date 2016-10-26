8ba83664e411b8dddf6400fa904a065b1e263d72
thibault daucourt <thibault.daucourt@he-arc.ch>
2016-10-26 13:41:37 +0200

    commit initial ajout de la page affichant les personnes

---
+++ src/java/ch/hearc/ig/odi/peoplelist/bean/PeopleListBean.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.peoplelist.bean;
+
+import ch.hearc.ig.odi.peoplelist.service.Services;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author thibault.daucourt
+ */
+@Named(value = "peopleListBean")
+@RequestScoped
+public class PeopleListBean implements Serializable{
+
+    @Inject Services services;
+    
+    /**
+     * Creates a new instance of PeopleListBean
+     */
+    public PeopleListBean() {
+    }
+    
+    public List getPeopleList(){
+        return services.getPeopleList();
+    }
+    
+}
